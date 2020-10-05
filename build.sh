6456cd285442973e3f4c73fbaa7ad0c818c4bccc
funesdli <71882930+funesdli@users.noreply.github.com>
2020-10-05 16:05:37 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+#dockerfile passé en argument : bash build.sh dockerfile
+echo -e "\nnom dockerfile : $1\n"
+
+#build
+docker build -t css/$1 - < $1
+docker pull mysql/mysql-server
+echo -e "\nnom de l'image : css/$1\n"
